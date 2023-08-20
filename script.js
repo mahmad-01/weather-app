873a1e746b6cf333cf8358886eca29f326f5d18f
mahmad-01 <mahmadbcs@gmail.com>
2023-08-20 11:39:24 -0300

    Location Search Functionality Created

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-async function checkWeather(){
-    const respone  = await fetch("https://api.weatherapi.com/v1/current.json?key=72d04cf270c44a22977125840231608&q=london", {mode:'cors'});
+const searchInput = document.querySelector('.searchBar');
+const subButton = document.getElementById('searchSubmit');
+
+async function checkWeather(location){
+    const respone  = await fetch("https://api.weatherapi.com/v1/current.json?key=72d04cf270c44a22977125840231608&q=" + location + "", {mode:'cors'});
     const weatherData = await respone.json();
     console.log(weatherData);
 }
 
-checkWeather();
+
+subButton.addEventListener("click", () => {
+    checkWeather(searchInput.value);
+})
+
+
+
+searchInput.addEventListener("click", ()=>{
+    searchInput.value = "";
+})
+
+
+
+/*now what do we need?
+- Search For Weather
+- Toggle F or C
+- Display Information
+- 
+
+*/
+
+
+
